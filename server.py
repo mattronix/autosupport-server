b19225fc918d8dae6956b2e591ba138c1937d449
Matthew Frost <m.frost@mattronix.nl>
2014-08-15 21:55:28 +0200

    Seperated POST and GET

--- server.py
+++ server.py
@@ -3,33 +3,24 @@ from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/matthew/asup/'
-ALLOWED_EXTENSIONS = set(['txt', 'tar.gz', '.gz', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_file(file.filename):
+        if file :
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return "File %s has been uploaded"%  filename
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+@app.route('/', methods=['GET'])
+def welcome():
+    return "Asup API V0.0.2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
